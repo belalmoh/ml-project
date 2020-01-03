8a541307f9e875fe975e03a086df8d4d85645755
Belal Mohammed <belal.mohamed@acu.edu.eg>
2020-01-03 22:12:52 +0200

    [DONE] used K as variable parameter

--- client/src/containers/movies/index.js
+++ client/src/containers/movies/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import Button from "@material-ui/core/Button";
 import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 import Papa from 'papaparse';
 
@@ -11,6 +13,7 @@ export default function Movies({data}) {
   const [open, setOpen] = React.useState(false);
   const [progress, setProgress] = React.useState(false);
   const [movieId, setMovieId] = React.useState(false);
+  const [kNeighbors, setKNeighbors] = React.useState(5);
 
   const [knnResult, setKnnResult] = React.useState([]);
 
@@ -23,14 +26,18 @@ export default function Movies({data}) {
     setOpen(false);
   };
 
+  const handleKNeighborsChange = ({target: {value}}) => {
+    setKNeighbors(value);
+  }
+
   useEffect(() => {
     setKnnResult([]);
-  }, [movieId])
+  }, [movieId, kNeighbors])
   
   const getSimilarMoviesUsingKNN = () => {
     setProgress(true);
     setKnnResult([]);
-    Papa.parse(`http://localhost:5000/api/algorithm/collab/movie/${movieId}/5`, {
+    Papa.parse(`http://localhost:5000/api/algorithm/collab/movie/${movieId}/${kNeighbors}`, {
       download: true,
       complete: (results, file) => {
         setProgress(false);
@@ -61,11 +68,17 @@ export default function Movies({data}) {
               <Modal open={open} handleClose={handleClose}>
                   <div>
                     <h2 id="transition-modal-title">Collaborative Approach</h2>
-                    <Button variant="outlined" color="secondary" onClick={() => getSimilarMoviesUsingKNN()}>
-                      {progress ? 
-                        <CircularProgress color="secondary" size={25} /> : `KNN` 
-                      }
-                    </Button>
+                    <Grid container spacing={6}>
+                      <Grid item xs={6}>
+                        <Button variant="outlined" color="secondary" onClick={() => getSimilarMoviesUsingKNN()}>
+                          {progress ? 
+                            <CircularProgress color="secondary" size={25} /> : `KNN` 
+                          }
+                        </Button>
+                        <br />
+                        <TextField id="standard-basic" label="K Value" onChange={handleKNeighborsChange} defaultValue={kNeighbors} disabled={progress}/>
+                      </Grid>
+                    </Grid>
                     <br />
                     {knnResult.length > 0 && !progress ? 
                       <Movies data={knnResult}/>
